4f30eb797c97ecb3efc2f66dd021347ca912806c
rrajasekaran <friends4you@gmail.com>
2015-08-26 20:52:36 +0530

    Create DBConstants.java

---
+++ src/main/java/com/adobe/db/stagevsprod/DBConstants.java
@@ -0,0 +1,39 @@
+package com.adobe.db.stagevsprod.Constants;
+
+public interface DBConstants {
+	
+	public String[] PRODUCT_NAME = {
+			
+								   "Acrobat", 
+								   "AfterEffect", 
+								   "Anywhere",
+								   "Audition", 
+								   "Captivate", 
+								   "CharacterAnimator",
+								   "CreativeCloud", 
+								   "Director", 
+								   "DesignSuite", 
+								   "Dreamweaver", 
+								   "Edge", 
+								   "ELearning", 
+								   "Encore", 
+								   "Firework", 
+								   "FlashPro", 
+								   "FlashBuilder", 
+								   "FrameMaker", 
+								   "Illustrator", 
+								   "InCopy", 
+								   "InDesign", 
+								   "Lightroom", 
+								   "Muse", 
+								   "Presenter", 
+								   "Photoshop", 
+								   "Premiere", 
+								   "Prelude", 
+								   "RoboHelp", 
+								   "SpeedGrade", 
+								   "TechnicalCommunicationSuite"
+								   
+	};
+
+}
